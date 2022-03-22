71c26e75094be0228639c0d2be2ac996c4f01666
kp <karthik.hy004@gmail.com>
2022-03-22 23:23:42 +0530

    code fix and formating

--- src/main/java/com/rbat/radarTrackEmulator/RadarTrackEmulatorApplication.java
+++ src/main/java/com/rbat/radarTrackEmulator/RadarTrackEmulatorApplication.java
@@ -1,16 +1,30 @@
 package com.rbat.radarTrackEmulator;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RadarTrackEmulatorApplication  {
+public class RadarTrackEmulatorApplication implements CommandLineRunner {
+
+	@Autowired
+	RandomDataGenerationSimulator randomDataGenerationSimulator;
+
+	private Logger logger = LoggerFactory.getLogger(RadarTrackEmulatorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RadarTrackEmulatorApplication.class, args);
+	}
 
-		new RandomDataGenerationSimulator().start();
+	@Override
+	public void run(String... args) throws Exception {
+
+		logger.info("run function starts >>");
+
+		randomDataGenerationSimulator.start();
 
 	}
 }
