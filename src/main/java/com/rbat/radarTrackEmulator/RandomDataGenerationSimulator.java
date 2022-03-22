78018c1e9693ccb21d369d0917873dfde831d6f9
kp <karthik.hy004@gmail.com>
2022-03-22 23:14:58 +0530

    code fix and formating

--- src/main/java/com/rbat/radarTrackEmulator/RandomDataGenerationSimulator.java
+++ src/main/java/com/rbat/radarTrackEmulator/RandomDataGenerationSimulator.java
@@ -5,16 +5,21 @@ import com.rbat.radarTrackEmulator.service.RadarTrackService;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomDataGenerationSimulator {
+
+  private Logger logger = LoggerFactory.getLogger(RandomDataGenerationSimulator.class);
 
   Random r;
   DecimalFormat coordinate;
@@ -26,7 +31,7 @@ public class RandomDataGenerationSimulator {
   int heading;
   String area_type;
 
-  List<Radartrack> dataList;
+  Radartrack radartrack;
 
   @Autowired
   RadarTrackService radarTrackService;
@@ -45,16 +50,15 @@ public class RandomDataGenerationSimulator {
   }
 
   private class LoopTask extends TimerTask {
+
     public void run() {
 
-      dataList = new ArrayList<>();
-
-      Radartrack radartrack = new Radartrack();
-      for(int i=0;i<=3;i++) {
+      radartrack = new Radartrack();
+      for (int i = 1; i <= 3; i++) {
+        List<Radartrack> dataList = new ArrayList<Radartrack>(i);
         switch (i) {
           case 1:
-            System.out.println("***************************Start***************************");
-
+            logger.debug("Type : Air");
             r = new Random();
             DecimalFormat coordinate = new DecimalFormat("#.####");
             coordinate.setRoundingMode(RoundingMode.CEILING);
@@ -62,82 +66,54 @@ public class RandomDataGenerationSimulator {
             speeding = new DecimalFormat("#.##");
             speeding.setRoundingMode(RoundingMode.CEILING);
 
-            id = 	Long.valueOf(r.nextInt(900) + 100);
+            id = Long.valueOf(r.nextInt(900) + 100);
             radartrack.setId(id);
-            System.out.println("ID :"+id);
-            System.out.println("----------------------");
 
             latitude = r.nextDouble();
             radartrack.setLatitude(Double.parseDouble(coordinate.format(latitude)));
-            System.out.println("Latitude :"+coordinate.format(latitude));
-            System.out.println("----------------------");
 
             longitude = r.nextDouble();
             radartrack.setLongitude(Double.parseDouble(coordinate.format(longitude)));
-            System.out.println("Longitude :"+coordinate.format(longitude));
-            System.out.println("----------------------");
 
             speed = r.nextDouble();
             radartrack.setSpeed(Double.parseDouble(speeding.format(speed)));
-            System.out.println("Speed :"+speeding.format(speed));
-            System.out.println("----------------------");
 
             heading = r.nextInt(360);
             radartrack.setHeading(heading);
-            System.out.println("Heading Direction:"+heading);
-            System.out.println("----------------------");
 
             radartrack.setArea_types("Air");
-            System.out.println("Type :"+"Air");
-
             dataList.add(radartrack);
-            System.out.println("***************************End***************************");
             break;
 
           case 2:
-            System.out.println("***************************Start***************************");
-
+            logger.debug("Type : Sea");
             coordinate = new DecimalFormat("#.####");
             coordinate.setRoundingMode(RoundingMode.CEILING);
 
             speeding = new DecimalFormat("#.##");
             speeding.setRoundingMode(RoundingMode.CEILING);
 
-            id = 	Long.valueOf(r.nextInt(900) + 100);
+            id = Long.valueOf(r.nextInt(900) + 100);
             radartrack.setId(id);
-            System.out.println("ID :"+id);
-            System.out.println("----------------------");
 
             latitude = r.nextDouble();
             radartrack.setLatitude(Double.parseDouble(coordinate.format(latitude)));
-            System.out.println("Latitude :"+coordinate.format(latitude));
-            System.out.println("----------------------");
 
             longitude = r.nextDouble();
             radartrack.setLongitude(Double.parseDouble(coordinate.format(longitude)));
-            System.out.println("Longitude :"+coordinate.format(longitude));
-            System.out.println("----------------------");
 
             speed = r.nextDouble();
             radartrack.setSpeed(Double.parseDouble(speeding.format(speed)));
-            System.out.println("Speed :"+speeding.format(speed));
-            System.out.println("----------------------");
 
             heading = r.nextInt(360);
             radartrack.setHeading(heading);
-            System.out.println("Heading Direction:"+heading);
-            System.out.println("----------------------");
 
             radartrack.setArea_types("Sea");
-            System.out.println("Type :"+"Sea");
-
             dataList.add(radartrack);
-            System.out.println("***************************End***************************");
             break;
 
           case 3:
-            System.out.println("***************************Start***************************");
-
+            logger.debug("Type : Ground");
             Random r = new Random();
             coordinate = new DecimalFormat("#.####");
             coordinate.setRoundingMode(RoundingMode.CEILING);
@@ -145,55 +121,42 @@ public class RandomDataGenerationSimulator {
             speeding = new DecimalFormat("#.##");
             speeding.setRoundingMode(RoundingMode.CEILING);
 
-            id = 	Long.valueOf(r.nextInt(900) + 100);
+            id = Long.valueOf(r.nextInt(900) + 100);
             radartrack.setId(id);
-            System.out.println("ID :"+id);
-            System.out.println("----------------------");
 
             latitude = r.nextDouble();
             radartrack.setLatitude(Double.parseDouble(coordinate.format(latitude)));
-            System.out.println("Latitude :"+coordinate.format(latitude));
-            System.out.println("----------------------");
 
             longitude = r.nextDouble();
             radartrack.setLongitude(Double.parseDouble(coordinate.format(longitude)));
-            System.out.println("Longitude :"+coordinate.format(longitude));
-            System.out.println("----------------------");
 
             speed = r.nextDouble();
             radartrack.setSpeed(Double.parseDouble(speeding.format(speed)));
-            System.out.println("Speed :"+speeding.format(speed));
-            System.out.println("----------------------");
 
             heading = r.nextInt(360);
             radartrack.setHeading(heading);
-            System.out.println("Heading Direction:"+heading);
-            System.out.println("----------------------");
 
             radartrack.setArea_types("Ground");
-            System.out.println("Type :"+"Ground");
-
             dataList.add(radartrack);
-            System.out.println("***************************End***************************");
             break;
 
-          default: System.out.println("No data");
+          default:
+            logger.debug("No data");
             break;
         }
-      }
-      System.out.println("Data List size : "+dataList.size());
 
-      for (Radartrack data : dataList) {
-        System.out.println(data.getId()+","
-            +data.getLatitude()+","
-            +data.getLongitude()+","
-            +data.getHeading()+","
-            +data.getSpeed()+","
-            +data.getArea_types());
-
-        radarTrackService.saveData(data);
+        for (int j = 0; j < dataList.toArray().length; j++) {
+          logger.info("-------Start-------");
+          logger.debug(dataList.get(j).getId() + ","
+              + dataList.get(j).getLatitude() + ","
+              + dataList.get(j).getLongitude() + ","
+              + dataList.get(j).getHeading() + ","
+              + dataList.get(j).getSpeed() + ","
+              + dataList.get(j).getArea_types());
+          radarTrackService.saveData(dataList.get(j));
+          logger.info("-------end-------");
+        }
       }
     }
   }
-
 }
